62b1f3c4981c898b80583e89f8b7efc8bbdc9b86
Mor Codou GUEYE <morcodou@gmail.com>
2022-07-10 23:59:17 -0400

    Complete CQRS Architecture

--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,11 +1,41 @@
+using System.Reflection;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Ordering.Application.Handlers.CommandHandlers;
+using Ordering.Core.Repositories.Command;
+using Ordering.Core.Repositories.Command.Base;
+using Ordering.Core.Repositories.Query;
+using Ordering.Core.Repositories.Query.Base;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Command;
+using Ordering.Infrastructure.Repositories.Command.Base;
+using Ordering.Infrastructure.Repositories.Query;
+using Ordering.Infrastructure.Repositories.Query.Base;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+// Configure for Sqlite
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<OrderingContext>(options => options.UseSqlite(defaultConnection));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
+});
+
+// Register dependencies
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMediatR(typeof(CreateCustomerHandler).GetTypeInfo().Assembly);
+builder.Services.AddScoped(typeof(IQueryRepository<>), typeof(QueryRepository<>));
+builder.Services.AddTransient<ICustomerQueryRepository, CustomerQueryRepository>();
+builder.Services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
+builder.Services.AddTransient<ICustomerCommandRepository, CustomerCommandRepository>();
 
 var app = builder.Build();
 
@@ -14,6 +44,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
 }
 
 app.UseHttpsRedirection();
